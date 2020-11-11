a06963dbe05ea5f9b2f5f2bf704356b0854c9d99
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-11 12:35:25 -0600

    Adding new DB associations between users and microposts

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
 
+    # adding new associations
+    belongs_to :user
+
     # adding new validations to the model
     validates :content, length: {maximum: 140}
     
--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+    # adding new associations
+    has_many :microposts
+    
 end
