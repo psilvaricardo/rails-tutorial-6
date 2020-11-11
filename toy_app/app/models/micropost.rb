5a9e87f76df2d292f5208bbc220a9545dddc29a7
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-11 13:41:21 -0600

    adding more validations

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -4,6 +4,6 @@ class Micropost < ApplicationRecord
     belongs_to :user
 
     # adding new validations to the model
-    validates :content, length: {maximum: 140}
+    validates :content, length: {maximum: 140}, presence :true
     
 end
