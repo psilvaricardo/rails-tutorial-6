511501427a01664e0a95b24ddf79ccb55250d506
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-11 13:54:55 -0600

    fixing weird syntax error

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -4,6 +4,7 @@ class Micropost < ApplicationRecord
     belongs_to :user
 
     # adding new validations to the model
-    validates :content, length: {maximum: 140}, presence :true
+    validates :content, length: {maximum: 140}, 
+                        presence: true
     
 end
