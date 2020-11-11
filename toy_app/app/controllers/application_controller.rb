2873992a6b8d30170e7cbccbf0de42d472b6422f
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-11 11:48:59 -0600

    reroute from hello world to the User's index page

--- toy_app/app/controllers/application_controller.rb
+++ toy_app/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "hello world!!"
-    end
+    
+    #def hello
+    #    render html: "hello world!!"
+    #end
+
+    
 end
--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
     # new route
-    root 'application#hello'
+    # root 'application#hello'
+
+    # new rute, reroute from hello world to the User's index page
+    root 'users#index'
 end
