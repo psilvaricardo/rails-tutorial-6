4a74fea57796b071ed8f201cc06f0fc73d9b1f16
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-13 14:51:18 -0600

    Adding validation and new index to the User Model plus DB migration

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ApplicationRecord
+
+    before_save { self.email = email.downcase  }
+    validates :name, presence: true, length: { maximum: 50 }
+
+=begin
+    To help understand where this comes from, we break it into pieces:
+
+    /               start of regex
+    \A              match start of a string
+    [\w+\-.]+       at least one word character, plus, hyphen, or dot
+    @               literal “at sign”
+    [a-z\d\-.]+     at least one letter, digit, hyphen, or dot
+    \.              literal dot
+    [a-z]+          at least one letter
+    \z              match end of a string
+    /               nend of regex
+    i               case-insensitive
+=end
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                        format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: true
+
 end
