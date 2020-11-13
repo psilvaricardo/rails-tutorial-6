989db8a77582746283c3b8e247d1f1cf957c1819
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-12 22:14:04 -0600

    Adding tests for the Sign up page

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -2,9 +2,10 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get new" do
+  test "should get sign up" do
     get signup_path
     assert_response :success
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 
 end
