b6440df9cd62d7e16bd8222136fccf5fdbb69edb
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-15 16:44:17 -0600

    Adding minimum password standards

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -23,5 +23,6 @@ class User < ApplicationRecord
                         uniqueness: true
     
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
     
 end
--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -53,13 +53,23 @@ class UserTest < ActiveSupport::TestCase
   duplicate_user = @user.dup
   @user.save
   assert_not duplicate_user.valid?
-end
+ end
 
-test "email addresses should be saved as lowercase" do
+ test "email addresses should be saved as lowercase" do
   mixed_case_email = "Foo@ExAMPle.CoM"
   @user.email = mixed_case_email
   @user.save
   assert_equal mixed_case_email.downcase, @user.reload.email
-end
+ end
+
+ test "password should be present (nonblank)" do
+  @user.password = @user.password_confirmation = " " * 6
+  assert_not @user.valid?
+ end
+
+ test "password should have a minimum length" do
+  @user.password = @user.password_confirmation = "a" * 5
+  assert_not @user.valid?
+ end
 
 end
