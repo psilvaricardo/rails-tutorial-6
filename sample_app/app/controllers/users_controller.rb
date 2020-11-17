b4ce718a9a91d48d85d1b4f5a2e06f033eb39adc
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-17 10:17:42 -0600

    Example of how to use the debugger

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
+    # Whenever you’re confused about something in a Rails application, 
+    # it’s a good practice to put debugger close to the code you think 
+    # might be causing the trouble.
+    # debugger
   end
 
   def new
