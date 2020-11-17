3804711958faac037cec2059080c6572d3d65289
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-17 16:13:44 -0600

    Handle a successful save action for User's Form
    Also adding the flash message

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -20,6 +20,8 @@ class UsersController < ApplicationController
 
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
