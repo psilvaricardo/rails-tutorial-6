879620736690d86645264537b1d410231c6ca9cc
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-18 15:33:00 -0600

    Remembering the user in a permanent way
    We will use a token for this, also updating current_user for persistent sessions

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -9,10 +9,23 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # Returns the current logged-in user (if any).
+  # Remembers a user in a persistent session.
+  def remember(user)
+    User.remember
+    cookies.permanent.encrypted[:user_id] = User.id
+    cookies.permanent[:remember_token] = User.remember_token
+  end
+
+  # Returns the user corresponding to the remember token cookie.
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && User.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -26,5 +39,5 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
 end
