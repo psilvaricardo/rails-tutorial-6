ec3a6a3494ca83316b15af44650662e836349b23
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-18 11:55:43 -0600

    Finding and Authenticating a User

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,10 +5,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    # params[:session][:email] has the submitted email address and
+    # params[:session][:password] has the submitted password.
+    # we already have exactly what we need: the User.find_by method
+
+    # This pulls the user out of the DB using the email address.
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    # if user is valid and can be authenticated
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      render 'new'
+    end
   end
 
   def destroy
   end
-  
+
 end
