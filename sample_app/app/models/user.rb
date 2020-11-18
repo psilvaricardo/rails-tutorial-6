8274f92dcb33bc84e85d9db4eccc549f3a91736a
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-18 14:58:18 -0600

    Defining the new token and digest methods using class << self

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -33,15 +33,20 @@ class User < ApplicationRecord
         BCrypt::Password.create(string, cost: cost)
     end
 
-    # Returns a random token.
-    def self.new_token
-        SecureRandom.urlsafe_base64
-    end
+    class << self
+        
+        # Returns the hash digest of the given string.
+        def digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                          BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+        end
+    
+        # Returns a random token.
+        def new_token
+          SecureRandom.urlsafe_base64
+        end
 
-    # Remembers a user in the database for use in persistent sessions.
-    def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
     end
 
 end
