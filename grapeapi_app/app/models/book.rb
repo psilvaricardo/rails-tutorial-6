48c127de1c6debfd0119d88cf4cac31e69d6bc3c
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-20 18:49:54 -0600

    Deleting the models and having a fresh start

--- grapeapi_app/app/models/book.rb
+++
@@ -1,6 +0,0 @@
-class Book < ApplicationRecord
-    # setting up associations
-    # https://guides.rubyonrails.org/association_basics.html
-    has_one :publisher
-    has_one :category
-end
--- grapeapi_app/app/models/category.rb
+++
@@ -1,9 +0,0 @@
-class Category < ApplicationRecord
-
-    # This fix is specific for DBs like SQLite not PostgreSQL nor MySQL.
-    serialize :category, Array
-
-    # setting up associations
-    # https://guides.rubyonrails.org/association_basics.html
-    belongs_to :book
-end
