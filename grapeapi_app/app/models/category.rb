1cc9c6545b3bc7b9095a1f5845323652d61cc27a
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-20 18:22:21 -0600

    Adding serialization for the category field in a Category model
    This solution is specific for databases like SQLite not PostgreSQL or MySQL

--- grapeapi_app/app/models/category.rb
+++ grapeapi_app/app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
+
+    # This fix is specific for DBs like SQLite not PostgreSQL nor MySQL.
+    serialize :category, Array
+
     # setting up associations
     # https://guides.rubyonrails.org/association_basics.html
     belongs_to :book
--- grapeapi_app/db/migrate/20201120235749_create_categories.rb
+++ grapeapi_app/db/migrate/20201120235749_create_categories.rb
@@ -1,11 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
-      t.string, :text
-      t.string :array
-      t.string :true,
-      t.string :default
-      t.string :[]
+      t.text :category
 
       t.timestamps
     end
