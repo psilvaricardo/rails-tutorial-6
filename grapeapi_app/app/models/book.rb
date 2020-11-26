e64f1bf941b4cd63f527531bd6c7db58e4eee6f5
Ricardo Plascencia Silva <ricardo_plascencia@epam.com>
2020-11-26 13:18:06 -0600

    Setting up asc between the two data models

--- grapeapi_app/app/models/book.rb
+++ grapeapi_app/app/models/book.rb
@@ -4,5 +4,6 @@ class Book < ApplicationRecord
     # https://guides.rubyonrails.org/association_basics.html
     # https://github.com/rails/rails/issues/34454 
     belongs_to :publisher, optional: true
+    has_and_belongs_to_many :categories
 
 end
